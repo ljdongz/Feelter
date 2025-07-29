0ff566c3bfd51d8e74555e5ee350347586830a71
Jeong Dong <eejxdong99@gmail.com>
2025-07-29 18:18:00 +0900

    [#3] Color Asset 추가

---
+++ Feelter/Sources/Shared/DesignSystem/Component/FloatingTitleTextField.swift
@@ -0,0 +1,20 @@
+//
+//  FloatingTitleTextField.swift
+//  Feelter
+//
+//  Created by 이정동 on 7/29/25.
+//
+
+import UIKit
+
+class FloatingTitleTextField: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
