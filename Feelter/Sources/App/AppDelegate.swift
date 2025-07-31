e1c42ecb6b57ddeef2cee5487ba196082d8e214e
Jeong Dong <eejxdong99@gmail.com>
2025-07-31 20:18:31 +0900

    [#7] DIContainer 구현

--- Feelter/Sources/App/AppDelegate.swift
+++ Feelter/Sources/App/AppDelegate.swift
@@ -15,6 +15,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         KakaoSDK.initSDK(appKey: AppConfiguration.kakaoApiKey)
+        DIContainer.shared.registerDependencies()
         
         return true
     }
---
+++ Feelter/Sources/App/DIContainer.swift
@@ -0,0 +1,56 @@
+//
+//  DIContainer.swift
+//  Feelter
+//
+//  Created by 이정동 on 7/31/25.
+//
+
+import Foundation
+
+final class DIContainer {
+    public static let shared = DIContainer()
+    private var dependencies: [String: Any] = [:]
+    
+    private init() {}
+    
+    func register<T, U>(_ dependency: T, type: U.Type) {
+        let key = String(describing: U.self)
+        dependencies[key] = dependency
+    }
+    
+    func resolve<T>(_ type: T.Type) -> T {
+        let key = String(describing: type)
+        let dependency = dependencies[key]
+        
+        guard let dependency = dependency as? T else {
+            fatalError("\(key)는 register되지 않았어어요. resolve 부르기전에 register 해주세요")
+        }
+        
+        return dependency
+    }
+}
+
+@propertyWrapper
+struct Dependency<T> {
+    let wrappedValue: T
+    
+    init() {
+        self.wrappedValue = DIContainer.shared.resolve(T.self)
+    }
+}
+
+extension DIContainer {
+    func registerDependencies() {
+        let appleAuthService = AppleAuthServiceImpl()
+        let kakaoAuthService = KakaoAuthServiceImpl()
+        let networkProvider = NetworkProviderImpl()
+        
+        let authRepository = AuthRepositoryImpl(
+            appleAuthService: appleAuthService,
+            kakaoAuthService: kakaoAuthService,
+            networkProvider: networkProvider
+        )
+        
+        register(authRepository, type: AuthRepository.self)
+    }
+}
