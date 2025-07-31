219b161384dacb526818bf502697f48d9ce0e9df
Jeong Dong <eejxdong99@gmail.com>
2025-07-31 17:01:31 +0900

    [#5] AuthRepository 구현

--- Feelter/Sources/Data/Repository/Implement/AuthRepositoryImpl.swift
+++ Feelter/Sources/Data/Repository/Implement/AuthRepositoryImpl.swift
@@ -8,19 +8,43 @@
 import Foundation
 
 struct AuthRepositoryImpl: AuthRepository {
+    
+    private let appleAuthService: AppleAuthService
+    private let kakaoAuthService: KakaoAuthService
+    
+    init(
+        appleAuthService: AppleAuthService,
+        kakaoAuthService: KakaoAuthService
+    ) {
+        self.appleAuthService = appleAuthService
+        self.kakaoAuthService = kakaoAuthService
+    }
+    
     func signInWithApple(
         idToken: String,
         deviceToken: String?,
         nickname: String?
     ) async throws {
-        
+        do {
+            let result = try await appleAuthService.requestAuthorization()
+            
+            // TODO: 서버로 토큰 전송
+        } catch {
+            
+        }
     }
     
     func signInWithKakao(
         oauthToken: String,
         deviceToken: String?
     ) async throws {
-        
+        do {
+            let result = try await kakaoAuthService.requestAuthorization()
+            
+            // TODO: 서버로 토큰 전송
+        } catch {
+            
+        }
     }
     
     
