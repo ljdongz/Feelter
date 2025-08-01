3834b1169357be99d514e00cdc46c47f245b04c7
Jeong Dong <eejxdong99@gmail.com>
2025-08-01 16:10:01 +0900

    [#7] ValidationEmail DTO 정의

---
+++ Feelter/Sources/Data/DTO/Auth/Request/ValidationEmailRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  ValidationEmailRequestDTO.swift
+//  Feelter
+//
+//  Created by 이정동 on 8/1/25.
+//
+
+import Foundation
+
+struct ValidationEmailRequestDTO: Encodable {
+    let email: String
+}
