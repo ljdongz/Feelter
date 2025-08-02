35d1e06b17ffb4a19fdcd5c6c5b0b9f23781fc18
Jeong Dong <eejxdong99@gmail.com>
2025-08-03 00:54:00 +0900

    [#9] 토큰 인터셉터 구현

--- Feelter/Sources/Core/Network/Base/NetworkError.swift
+++ Feelter/Sources/Core/Network/Base/NetworkError.swift
@@ -8,8 +8,22 @@
 import Foundation
 
 enum NetworkError: Error {
-  case invalidURL
-  case httpStatusError(Int)
-  case urlSessionError(Error)
-  case decodingError(Error)
+    case invalidURL
+    case urlSessionError(Error)
+    case decodingError(Error)
+}
+
+enum HTTPResponseError: Error {
+    case invalidToken // 401
+    case forbidden // 403
+    case expiredAccessToken // 419
+    case invalidHeaderKey // 420
+    case overcallLimit // 429
+    case invalidAPI // 444
+    
+    case clientError(Int)
+    
+    case serverError(Int)
+    
+    case unknownError(Int)
 }
--- Feelter/Sources/Core/Network/NetworkProviderImpl.swift
+++ Feelter/Sources/Core/Network/NetworkProviderImpl.swift
@@ -8,42 +8,82 @@
 import Foundation
 
 struct NetworkProviderImpl: NetworkProvider {
-  
-  private let session: URLSession
-  
-  init(session: URLSession = .shared) {
-    self.session = session
-  }
-  
-  func request<T: Decodable>(endpoint: APIEndpoint, type: T.Type) async throws -> T {
-    let data = try await requestData(endpoint: endpoint)
-    do {
-      let decodedData = try JSONDecoder().decode(T.self, from: data)
-      return decodedData
-    } catch {
-        throw NetworkError.decodingError(error)
+    
+    private let session: URLSession
+    private let tokenInterceptor: RequestInterceptor?
+    
+    init(
+        session: URLSession = .shared,
+        tokenInterceptor: RequestInterceptor? = nil
+    ) {
+        self.session = session
+        self.tokenInterceptor = tokenInterceptor
     }
-  }
+    
+    func request<T: Decodable>(endpoint: APIEndpoint, type: T.Type) async throws -> T {
+        // URLRequest 객체 생성
+        guard var request = endpoint.asURLRequest() else {
+            throw NetworkError.invalidURL
+        }
+        
+        // 토큰 인터셉터를 설정한 경우, adapt 호출
+        if let tokenInterceptor {
+            request = try await tokenInterceptor.adapt(request)
+        }
+        
+        do {
+            // API 요청
+            return try await performRequest(request: request, type: type)
+        } catch  {
+            // 토큰 인터셉터에 에러를 전달하여, 에러 상태에 따른 액세스 토큰 갱신 요청 처리
+            if let tokenInterceptor,
+               let retryRequest = try await tokenInterceptor.retry(request, for: error) {
+                return try await performRequest(request: retryRequest, type: type)
+            }
+            
+            throw error
+        }
+    }
 }
 
 private extension NetworkProviderImpl {
-  func requestData(endpoint: APIEndpoint) async throws -> Data {
-    guard let request = endpoint.asURLRequest() else {
-      throw NetworkError.invalidURL
+    
+    func performRequest<T: Decodable>(request: URLRequest, type: T.Type) async throws -> T {
+        
+        // URLSession 통신
+        let (data, response): (Data, URLResponse)
+        do {
+            (data, response) = try await session.data(for: request)
+        } catch {
+            throw NetworkError.urlSessionError(error)
+        }
+        
+        // HTTP Response 코드 확인
+        if let httpResponse = response as? HTTPURLResponse {
+            guard 200...299 ~= httpResponse.statusCode else {
+                throw mapStatusCodeToError(httpResponse.statusCode)
+            }
+        }
+        
+        // JSON 디코딩
+        do {
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            throw NetworkError.decodingError(error)
+        }
     }
     
-    do {
-      let (data, response) = try await session.data(for: request)
-      
-      if let httpResponse = response as? HTTPURLResponse {
-        guard 200...299 ~= httpResponse.statusCode else {
-          throw NetworkError.httpStatusError(httpResponse.statusCode)
+    func mapStatusCodeToError(_ statusCode: Int) -> HTTPResponseError {
+        switch statusCode {
+        case 401: .invalidToken
+        case 403: .forbidden
+        case 419: .expiredAccessToken
+        case 420: .invalidHeaderKey
+        case 429: .overcallLimit
+        case 444: .invalidAPI
+        case 400..<500: .clientError(statusCode)
+        case 500..<600: .serverError(statusCode)
+        default: .unknownError(statusCode)
         }
-      }
-      
-      return data
-    } catch {
-      throw NetworkError.urlSessionError(error)
     }
-  }
 }
