a3f7bf345c27f0acdb1a2a463631a2f0c2b609b9
Jeong Dong <eejxdong99@gmail.com>
2025-08-10 13:58:21 +0900

    [#19] 내비게이션 우측 버튼 설정 코드 수정

--- Feelter/Sources/Feature/Detail/FilterDetailViewController.swift
+++ Feelter/Sources/Feature/Detail/FilterDetailViewController.swift
@@ -14,10 +14,30 @@ final class FilterDetailViewController: RxBaseViewController {
 
     private let mainView = FilterDetailView()
     
+    private lazy var navigationRightBarButton: UIButton = {
+        let view = UIButton()
+        view.setImage(.likeEmpty, for: .normal)
+        view.frame = CGRect(x: 0, y: 0, width: 32, height: 32)
+        view.tintColor = .gray15
+        view.addTarget(self, action: #selector(rightBarButtonTapped), for: .touchUpInside)
+        return view
+    }()
+    
     private let viewModel = FilterDetailViewModel()
+    
+    private var isBookmarked = false
+    private var rightBarButtonImage: UIImage {
+        isBookmarked ? .likeFill : .likeEmpty
+    }
     
     override func loadView() {
         view = mainView
+    }
+    
+    override func setupView() {
+        view.backgroundColor = .gray100
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: navigationRightBarButton)
     }
     
     override func bind() {
@@ -26,3 +46,19 @@ final class FilterDetailViewController: RxBaseViewController {
         let output = viewModel.transform(input: input)
     }
 }
+
+private extension FilterDetailViewController {
+    @objc private func rightBarButtonTapped() {
+        isBookmarked.toggle()
+        navigationRightBarButton.setImage(rightBarButtonImage, for: .normal)
+    }
+}
+
+
+#if DEBUG
+import SwiftUI
+@available(iOS 17.0, *)
+#Preview {
+    UINavigationController(rootViewController: FilterDetailViewController())
+}
+#endif
