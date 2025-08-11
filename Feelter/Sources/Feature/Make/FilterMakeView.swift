af185549c19e89b4e39adbad7ab529b0f1a1d27a
Jeong Dong <eejxdong99@gmail.com>
2025-08-11 16:37:45 +0900

    [#21] FilterMake 기본 구조 구현

---
+++ Feelter/Sources/Feature/Make/FilterMakeView.swift
@@ -0,0 +1,23 @@
+//
+//  FilterMakeView.swift
+//  Feelter
+//
+//  Created by 이정동 on 8/11/25.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+final class FilterMakeView: BaseView {
+
+    override func setupSubviews() {
+        
+    }
+    
+    override func setupConstraints() {
+        
+    }
+}
---
+++ Feelter/Sources/Feature/Make/FilterMakeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FilterMakeViewController.swift
+//  Feelter
+//
+//  Created by 이정동 on 8/11/25.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+
+final class FilterMakeViewController: RxBaseViewController {
+
+    let mainView = FilterMakeView()
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    override func setupView() {
+        title = "Make"
+    }
+    
+    override func bind() {
+        
+    }
+}
