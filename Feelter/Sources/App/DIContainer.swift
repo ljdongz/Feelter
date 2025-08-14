1435e440ded8629eaeaa5874944bbef7beebbdb2
Jeong Dong <eejxdong99@gmail.com>
2025-08-14 21:31:39 +0900

    [#23] ChatRepository 의존성 등록

--- Feelter/Sources/App/DIContainer.swift
+++ Feelter/Sources/App/DIContainer.swift
@@ -66,10 +66,14 @@ extension DIContainer {
         let bannerRepository = BannerRepositoryImpl(
             networkProvider: networkProvider
         )
+        let chatRepository = ChatRepositoryImpl(
+            networkProvider: networkProvider
+        )
         
         register(authRepository, type: AuthRepository.self)
         register(userRepository, type: UserRepository.self)
         register(filterRepository, type: FilterRepository.self)
         register(bannerRepository, type: BannerRepository.self)
+        register(chatRepository, type: ChatRepository.self)
     }
 }
