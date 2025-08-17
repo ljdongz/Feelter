e9d87ae37598b8243c0c60a4f6fb10a24424a3ff
Jeong Dong <eejxdong99@gmail.com>
2025-08-17 22:01:23 +0900

    [#25] Push Notification 설정

--- Feelter/Sources/App/AppDelegate.swift
+++ Feelter/Sources/App/AppDelegate.swift
@@ -17,6 +17,9 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         KakaoSDK.initSDK(appKey: AppConfiguration.kakaoApiKey)
         DIContainer.shared.registerDependencies()
         
+        configurePushNotification()
+        application.registerForRemoteNotifications()
+        
         return true
     }
 
@@ -33,7 +36,44 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+// MARK: - Remote Notification
+
+extension AppDelegate {
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        let token = deviceToken.reduce("") { $0 + String(format: "%02X", $1) }
+        print(token)
+    }
+
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    private func configurePushNotification() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.delegate = self
+        
+        // 알림을 표시하기 위한 승인을 요청
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
+            print("Permission granted: \(granted)")
+        }
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // foreground 상태일 때에도 알림 배너가 나오도록 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print(#function)
+        
+    }
+    
+    // 푸시 알림 배너 클릭했을 시 실행
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print(#function)
+    }
+}
