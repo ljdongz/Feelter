fd1b32078ea0b93768113b99d2fccba2033c17f8
Jeong Dong <eejxdong99@gmail.com>
2025-08-17 17:00:36 +0900

    [#25] SocketProvider 구현

---
+++ Feelter/Sources/Core/Network/Base/SocketProvider.swift
@@ -0,0 +1,13 @@
+//
+//  SocketIOProvider.swift
+//  Feelter
+//
+//  Created by 이정동 on 8/17/25.
+//
+
+import Foundation
+
+protocol SocketProvider {
+    func connect(roomID: String, receiveMessage: @escaping ((ChatMessage) -> Void))
+    func disconnect()
+}
