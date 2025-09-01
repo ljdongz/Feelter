445ba525b6d7caa1c6a99e48fcdedbbe0fe3e580
Jeong Dong <eejxdong99@gmail.com>
2025-09-01 20:29:29 +0900

    [#37] 사진접근 Info.plist 추가

---
+++ Feelter/Sources/Shared/DesignSystem/Component/PhotoPickerInputView.swift
@@ -0,0 +1,20 @@
+//
+//  PhotoPickerInputView.swift
+//  Feelter
+//
+//  Created by 이정동 on 9/1/25.
+//
+
+import UIKit
+
+class PhotoPickerInputView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
